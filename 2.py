f2960794efc5d87cf6aa570997d414d4e308a04f
Teddy427 <3549263184@qq.com>
2025-06-26 19:28:46 +0800

    add requirements.txt

--- 2.py
+++ 2.py
@@ -9,6 +9,9 @@ theta = pi / 3
 v = [[1],[0]]
 R = [[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]]
 v0 = np.dot(R, v)
+plt.figure(figsize = (6,6), dpi = 80)
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
 Plot(v)
 Plot(v0)
 plt.show()
